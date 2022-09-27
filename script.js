162ec0ad02bdd1cd418f7700f9998b0eb5da3fdf
Micheal <oguntayomicheal@gmail.com>
2022-09-27 10:56:42 +0100

    Update script.js file

--- script.js
+++ script.js
@@ -14,43 +14,43 @@ function addToLocalStorage() {
   localStorage.setItem('storedBooks', stringifyArray);
 }
 
-function booksFilter(eachBook) {
-  bookArray = bookArray.filter((book) => book !== eachBook);
+function booksFilter(index) {
+  bookArray = bookArray.filter((book) => book !== bookArray[index]);
 }
 
 function displayBook() {
   const addedBooks = document.getElementById('list');
   addedBooks.innerHTML = '';
-
-  bookArray.forEach((eachBook) => {
+  for (let i = 0; i < bookArray.length; i += 1)  {
     const container = document.createElement('div');
     container.classList.add('book');
     addedBooks.appendChild(container);
 
     const bookTitle = document.createElement('p');
     bookTitle.classList.add('title');
-    bookTitle.textContent = eachBook.title;
+    bookTitle.textContent = bookArray[i].title;
     container.appendChild(bookTitle);
 
     const bookAuthor = document.createElement('p');
     bookAuthor.classList.add('author');
-    bookAuthor.textContent = eachBook.author;
+    bookAuthor.textContent = bookArray[i].author;
     container.appendChild(bookAuthor);
 
     const removeButton = document.createElement('button');
     removeButton.classList.add('remove');
     removeButton.textContent = 'remove';
 
-    removeButton.addEventListener('click', () => {
-      booksFilter(eachBook);
+    removeButton.onclick = () => {
+      booksFilter(i);
       addToLocalStorage();
       displayBook();
-    });
+    };
+
     container.appendChild(removeButton);
 
     const line = document.createElement('hr');
     container.appendChild(line);
-  });
+  };
 }
 
 function getFromLocalStorage() {
@@ -59,10 +59,14 @@ function getFromLocalStorage() {
   displayBook();
 }
 
-getFromLocalStorage();
+if (localStorage.getItem('storedBooks') == null) {
+  addToLocalStorage();
+} else {
+  getFromLocalStorage();
+}
 
 const addBtn = document.getElementById('addButton');
-addBtn.addEventListener('click', () => {
+  addBtn.addEventListener('click', () => {
   const title = document.getElementById('title');
   const author = document.getElementById('author');
   bookObj(title.value, author.value);
